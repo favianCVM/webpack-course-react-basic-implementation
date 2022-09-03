13727e02a00a0008d0be4a6709b8a95ef8298896
fabianSis <fabian@asistensi.com>
2022-09-03 19:33:00 -0400

    add react webpack configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./components/Button";
+import './styles/global.scss'
+
+const App = () => {
+	return (
+		<div className="app_body">
+			holamundo
+			<Button>soy un boton</Button>
+		</div>
+	);
+};
+
+export default App;
